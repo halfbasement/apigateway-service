64e99d86593da43edbceed06310dbedd54f2de8c
halfbasement <anyang0710@naver.com>
2022-10-04 21:01:51 +0900

    msa

--- src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
+++ src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -3,9 +3,10 @@ package com.example.apigatewayservice.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -27,7 +28,9 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
             log.info("Custom PRE filter: request id -> {}",request.getId());
 
-            return chain.filter(exchange).then();
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                log.info("Custom Post Filter: response code -> {} ",response.getStatusCode() );
+            }));
 
         };
     }
